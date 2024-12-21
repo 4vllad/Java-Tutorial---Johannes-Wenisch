ca2f7a096af7f0cbaacfd4da7c3ac54a232ced59
A <34423493+4vllad@users.noreply.github.com>
2024-12-21 11:09:33 +0100

    Finishing the 1st Tutorial (Lambda)

--- JavaTeil1Lambda/src/main/java/org/example/Main.java
+++ JavaTeil1Lambda/src/main/java/org/example/Main.java
@@ -1,17 +1,57 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Comparator<String> comparatorAnonymeInnereKlasse = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s1.length() - s2.length();
+            }
+        };
+
+        String name1 = "Alex";
+        String name2 = "Hendrik";
+
+        System.out.println(comparatorAnonymeInnereKlasse.compare(name1,name2));
+
+
+        Comparator<String> comparatorLambda = (String s1, String s2) -> { return s1.length() - s2.length(); };
+        System.out.println(comparatorLambda.compare("Olaf", "Olga"));
+
+        Comparator<String> comparatorLambda2 = (s1, s2) -> {return s1.length() - s2.length();};
+        System.out.println(comparatorLambda2.compare("A", "ABC"));
+
+        Comparator<String> comparatorLambda3 = (s1,s2) -> s1.length() - s2.length();
+        System.out.println(comparatorLambda3.compare("Bro","Brodette"));
+
+        List<String> wörter = Arrays.asList("Za", "bZ", "Boooo", "A", "B", "CD", "Dude", "Guy");
+
+        System.out.println(wörter);
+
+        //Collections.sort(wörter,comparatorAnonymeInnereKlasse);
+        //Collections.sort(wörter,comparatorLambda);
+        //Collections.sort(wörter,comparatorLambda2);
+        Collections.sort(wörter,comparatorLambda3);
+
+        System.out.println(wörter);
+
+        Predicate<String> wörterLängerAlsFünfBuchstaben = (String s) -> s.length()>5;
+
+        wörterLängerAlsFünfBuchstaben = (s) -> s.length() > 5;
+        wörterLängerAlsFünfBuchstaben = s -> s.length() > 5;
+        System.out.println(wörterLängerAlsFünfBuchstaben.test("hey"));
+        System.out.println(wörterLängerAlsFünfBuchstaben.test("hey whey"));
+
+        Runnable runnable = () -> System.out.println("Hallo Welt!");
+
+        runnable.run();
     }
 }
